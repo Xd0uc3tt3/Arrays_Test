a7bc897e5fec70e8bd614a22a7f383eb8f0c99e2
XD0uc3tt3 <doucettexander@gmail.com>
2025-09-17 14:10:10 -0300

    Implemented Array

--- Program.cs
+++ Program.cs
@@ -8,8 +8,31 @@ namespace ArraysProgramming1
 {
     internal class Program
     {
+        static string[] partyNames = { "Member1", "Member2", "Member3", "Member4" };
+        static int[] partyLevels = new int[4];
+
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine(partyNames[2]);
+
+            partyLevels[0] = 1;
+            partyLevels[1] = 1;
+            partyLevels[2] = 1;
+            partyLevels[3] = 1;
+
+            LevelUpPartyMember("Member2");
+
+            Console.ReadKey();
+
+        }
+
+        static void LevelUpPartyMember(string memberName)
+        {
+            int indexOfPartyMember = Array.IndexOf(partyLevels, memberName);
+            partyLevels[indexOfPartyMember] += 1;
+          
         }
     }
 }
